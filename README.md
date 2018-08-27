b2f81c2f258f61b1ab167656d2594a8dfd77d1b2
Daiki Saito <Daiki.Saito@isobar.com>
2018-08-27 19:39:07 +0900

    path修正

---
+++ README.md
@@ -0,0 +1,5 @@
+## page
+threeJSを用いたフォトギャラリーのプロトタイプ
+[https://dsflon.github.io/dsflon-three-gallery/](https://dsflon.github.io/dsflon-three-gallery/)
+
+[https://dsflon.github.io/dsflon-three-gallery/gallery.html](https://dsflon.github.io/dsflon-three-gallery/gallery.html)
--- docs/common/js/load.js
+++ docs/common/js/load.js
@@ -3,9 +3,9 @@ var ua = navigator.userAgent,
 	// uaSP = ua.indexOf("iPhone") >= 0 || ua.indexOf("iPad") >= 0;
 
 if(uaSP) {
-	var file = "/gallery/common/js/render_sp.js"
+	var file = "./common/js/render_sp.js"
 } else {
-	var file = "/gallery/common/js/render.js"
+	var file = "./common/js/render.js"
 }
 
 (function(){
